eb4dfa87f15c13ce01132825978af66d3e0eb206
Lau Thiam Kok <lau.thiamkok@yahoo.co.uk>
2014-11-04 14:58:29 +0000

    Example updated

--- jasmine/angular/spec/MainControllerSpec.js
+++ jasmine/angular/spec/MainControllerSpec.js
@@ -1,25 +1,71 @@
-describe('Unit: MainController', function() {
-  // Our tests will go here
+describe('SimpleController Test', function() {
+    
+    // Boilerplate starts from here...
+    // Load the module with MainController
+    beforeEach(module('myApp'));
+
+    var controller, scope;
   
-  // Load the module with MainController
-  beforeEach(module('myApp'));
-  
-  var ctrl, scope;
-  // inject the $controller and $rootScope services
-  // in the beforeEach block
-  beforeEach(inject(function($controller, $rootScope) {
-    // Create a new scope that's a child of the $rootScope
-    scope = $rootScope.$new();
-    // Create the controller
-    ctrl = $controller('MainController', {
-      $scope: scope
+    // inject the $controller and $rootScope services.
+    // in the beforeEach block.
+    beforeEach(inject(function ($rootScope, $controller) {
+        
+        // Create a new scope that's a child of the $rootScope.
+        scope = $rootScope.$new();
+        
+        // Create the controller.
+        controller = $controller;
+        controller("SimpleController", {$scope: scope});
+    }));
+    // ...Boilerplate ends here
+    
+    // Our tests will go here.
+    it("should assign message to hello world", function () {
+        expect(scope.message).toBe("Hello World");
     });
-  }));
   
-  it('should create $scope.greeting when calling sayHello', 
-    function() {
-      expect(scope.greeting).toBeUndefined();
-      scope.sayHello();
-      expect(scope.greeting).toEqual("Hello Ari");
-  });
+    it('should create $scope.greeting and say \'Hello World\' when calling sayHello method', function() {
+        expect(scope.greeting).toBeUndefined();
+        expect(scope.name).toEqual("World");
+        scope.sayHello();
+        expect(scope.greeting).toEqual("Hello World");
+    });
 });
+
+describe("SimpleService Test 1", function(){
+    
+    // Boilerplate starts from here...
+    beforeEach(module("myApp"));
+
+    var service;
+
+    beforeEach(inject(function(SimpleService){
+       service = SimpleService;
+    }));
+    // ...Boilerplate ends here
+    
+    // Our tests will go here.
+    describe("getData", function(){
+        it("should return an array of items", function(){
+            expect(service.getData()).toBeDefined();
+        });
+    });
+    
+    describe('when I call SimpleService.prop', function(){
+        it("returns Hello World", function(){
+            expect(service.prop).toEqual('Hello World');
+        });
+    });
+});
+
+describe("SimpleService Test 2", function(){
+
+    describe('when I call SimpleService.prop', function(){
+        beforeEach(module('myApp'));
+        it('returns Hello World', inject(function(SimpleService){ //parameter name = service name
+            expect( SimpleService.prop ).toEqual('Hello World');
+        }))
+    });
+    
+});
+
--- jasmine/angular/src/MainController.js
+++ jasmine/angular/src/MainController.js
@@ -1,6 +1,24 @@
-angular.module('myApp', []).controller('MainController', function($scope) {
-  $scope.name = "Ari";
-  $scope.sayHello = function() {
-    return $scope.greeting = "Hello " + $scope.name;
-  }
-})
+var app = angular.module("myApp", []);
+
+app.controller('SimpleController', function($scope) {
+    $scope.message = "Hello World";
+    $scope.name = "World";
+    $scope.sayHello = function() {
+        return $scope.greeting = "Hello " + $scope.name;
+    };
+});
+
+app.factory("SimpleService", function(){
+    var service = {
+        prop:'Hello World',
+        getData: function(){
+            return [{
+                id: 1,
+                name: "Mark"
+            }];
+        }
+
+    };
+
+    return service;
+});
